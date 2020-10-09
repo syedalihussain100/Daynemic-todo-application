401194fe5849ad2ec755c18f31fb9344ca9c3558
syedalihussain100 <syedalihussain100@gmail.com>
2020-10-09 22:57:57 +0500

    Changes in todo-application

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Todo from "./Todo";
 import "./App.css";
+import Happy from "./happy";
 
 function App() {
   return (
     <div>
+      <Happy />
+      <br/> <br/>
       <Todo />
     </div>
   );
--- src/happy.js
+++ src/happy.js
@@ -1,19 +1,18 @@
 import React, { Component } from "react";
 
-class Todo extends Component {
+class happy extends Component {
   constructor() {
     super();
-
     this.state = {
       todos: [
-        { title: "someting1", edit: false },
-        { title: "someting2", edit: false },
+        { title: "hammad", edit: false },
+        { title: "zaid", edit: false },
       ],
       value: "",
     };
   }
 
-  add_todo = () => {
+  handle_add = () => {
     let obj = { title: this.state.value };
     this.setState({
       todos: [...this.state.todos, obj],
@@ -21,69 +20,66 @@ class Todo extends Component {
     });
   };
 
-  delete_todo = (i) => {
+  handle_delete = (i) => {
     this.state.todos.splice(i, 1);
     this.setState({
       todos: this.state.todos,
     });
   };
 
-  edit_todo = (index) => {
-    this.state.todos[index].edit = true;
+  handle_edit = (i) => {
+    this.state.todos[i].edit = true;
     this.setState({
       todos: this.state.todos,
     });
   };
 
-  handle_change = (e, i) => {
+  handle_Change = (e, i) => {
     this.state.todos[i].title = e.target.value;
     this.setState({
       todos: this.state.todos,
     });
   };
 
-  update_todos = (i) => {
+  handle_Update = (i) => {
     this.state.todos[i].edit = false;
     this.setState({
       todos: this.state.todos,
     });
   };
-
   render() {
     const { todos, value } = this.state;
     return (
       <div>
-        <h1>Daynemic Todos Application !</h1>
+        <h2>Practise Todo-Application!</h2>
         <input
           value={value}
           onChange={(e) => this.setState({ value: e.target.value })}
           type="text"
-          placeholder="Enter Your Value!!"
+          placeholder="Enter Your Value"
         />
-        <button onClick={this.add_todo}>Click Your Data!</button>
-        <br /> <br />
+        <button onClick={this.handle_add}>Click Data</button>
+
         <ul>
           {todos.map((v, i) => {
             return (
               <li key={i}>
                 {v.edit ? (
                   <input
-                    value={v.title}
-                    onChange={(e) => this.handle_change(e, i)}
+                    onChange={(e) => this.handle_Change(e, i)}
                     type="text"
                     placeholder="Enter Your Edit Value!"
+                    value={v.title}
                   />
                 ) : (
                   v.title
                 )}
                 {v.edit ? (
-                  <button onClick={() => this.update_todos(i)}>Update</button>
+                  <button onClick={() => this.handle_Update(i)}>Update</button>
                 ) : (
-                  <button onClick={() => this.edit_todo(i, v.title)}>
-                    Edit
-                  </button>
+                  <button onClick={() => this.handle_edit(i)}>Edit</button>
                 )}
-                <button onClick={() => this.delete_todo(i)}>Delete</button>
+                <button onClick={() => this.handle_delete(i)}>Delete</button>
               </li>
             );
           })}
@@ -93,4 +89,5 @@ class Todo extends Component {
   }
 }
 
-export default Todo;
+export default happy;
+
